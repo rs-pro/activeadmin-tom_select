d799d740c6e7e758fddd6e9814e309636ee9116e
Gleb Tv <glebtv@gmail.com>
2025-07-13 22:41:23 +0300

    save

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,15 @@ require 'rspec/rails'
 require 'support/reset_settings'
 require 'support/active_admin_version'
 
+# Fix for Rails 8 compatibility - fixture_path= method removal
+if Rails.version.to_f >= 8.0 && !ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  class ActiveSupport::TestCase
+    class << self
+      attr_accessor :fixture_path
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
